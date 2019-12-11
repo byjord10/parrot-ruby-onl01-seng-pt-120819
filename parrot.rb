0fc7655f93677daaa0cad886df590bfd0fc59938
Brianna Jordan <bryvjordan9@gmail.com>
2019-12-11 22:29:24 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
-  puts "Squawk!"
-  return "Squawk!"
-end
-
 def parrot(phrase= "Pretty bird!")
-  puts phrase 
+  puts "Squawk!"
 end
